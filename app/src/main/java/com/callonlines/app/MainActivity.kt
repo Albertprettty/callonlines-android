ccff869ed7012b30f4e8ef8347eb174997c6e493
Albertprettty <53382208+Albertprettty@users.noreply.github.com>
2026-05-12 23:31:20 -0400

    Update MainActivity.kt

--- app/src/main/java/com/callonlines/app/MainActivity.kt
+++ app/src/main/java/com/callonlines/app/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.net.http.SslError
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.webkit.CookieManager
@@ -119,6 +118,8 @@ class MainActivity : AppCompatActivity() {
             useWideViewPort = true
             loadWithOverviewMode = true
             setGeolocationEnabled(false)
+            @Suppress("DEPRECATION")
+            saveFormData = true
         }
 
         if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
@@ -264,6 +265,7 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         webView.onPause()
+        CookieManager.getInstance().flush()
     }
 
     override fun onResume() {
@@ -272,6 +274,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
+        CookieManager.getInstance().flush()
         webView.stopLoading()
         webView.destroy()
         super.onDestroy()
